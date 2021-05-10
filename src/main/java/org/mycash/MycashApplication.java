8f1600991a8c4cbbf57667a5413be73fe47ae0d9
Daniel Petrico <d.petrico@ga.agr.br>
2021-05-10 16:04:08 -0300

    versão inicial, baixada do site start.spring.io

---
+++ src/main/java/org/mycash/MycashApplication.java
@@ -0,0 +1,13 @@
+package org.mycash;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MycashApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MycashApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/mycash/MycashApplicationTests.java
@@ -0,0 +1,13 @@
+package org.mycash;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MycashApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
